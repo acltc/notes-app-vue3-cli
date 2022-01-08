e02244571f58c14fcba8b6742acf6e55ad23e550
Peter Jang <peter.x.jang@gmail.com>
2022-01-08 11:42:53 -0600

    Split NoteContainer.vue into subcomponents

---
+++ src/components/NoteSelector.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="note-selector">
+    <p class="note-selector-title">First note...</p>
+    <p class="note-selector-timestamp">Timestamp here...</p>
+  </div>
+</template>
+
+<style scoped>
+/* LAYOUT */
+.note-selector {
+  padding: 1em;
+}
+.note-selector p {
+  margin: 0;
+}
+
+/* COLORS */
+.note-selector {
+  border-bottom: 1px solid #dcdadc;
+}
+.note-selector.active {
+  background-color: #fce18d;
+}
+.note-selector-title {
+  background-color: inherit;
+}
+.note-selector-timestamp {
+  color: #626262;
+  background-color: inherit;
+}
+
+/* TYPOGRAPHY */
+.note-selector-title {
+  font-weight: bold;
+}
+.note-selector-timestamp {
+  font-size: 0.7em;
+}
+</style>
---
+++ src/components/NoteSelectors.vue
@@ -0,0 +1,29 @@
+<script>
+import NoteSelector from "./NoteSelector.vue";
+
+export default {
+  components: {
+    NoteSelector,
+  },
+};
+</script>
+
+<template>
+  <div class="note-selectors">
+    <NoteSelector />
+    <NoteSelector />
+    <NoteSelector />
+  </div>
+</template>
+
+<style scoped>
+/* LAYOUT */
+.note-selectors {
+  flex: 0 0 13em;
+}
+
+/* COLORS */
+.note-selectors {
+  border-right: 1px solid #dcdadc;
+}
+</style>
